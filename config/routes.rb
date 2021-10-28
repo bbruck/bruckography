3cf6a9ddb9bb75474c1a5a690c054859ee9be430
Bernard Bruck <bernard.bruck@gmail.com>
2021-10-28 10:47:33 +0200

    Add pages & routes config

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,9 @@
 Rails.application.routes.draw do
   devise_for :users
   root to: 'pages#home'
+  get 'about', to: 'pages#about', as: :about
+  get 'series', to: 'pages#series', as: :series
+  get 'work', to: 'pages#work', as: :work
+  get 'contact', to: 'pages#contact', as: :contact
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
